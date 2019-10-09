81f397eca1d2b236ed94526e1f6e54fe370e8129
jordelucas <jordevalucas@hotmail.com>
2019-10-08 23:13:35 -0300

    #23 - adiciona contagem de quebra de linha

--- Av_2/Projeto_Av2_EDB/src/br/ufrn/imd/edb2/Huffman.java
+++ Av_2/Projeto_Av2_EDB/src/br/ufrn/imd/edb2/Huffman.java
@@ -6,18 +6,17 @@ import java.util.Map;
 
 
 public class Huffman {
-    //Coloquei pra retornar uma hashmap para poder fazer o teste
     public HashMap<Character, Integer> characterFrequency(String fileName) throws IOException {
-        //Adiciona leitura de arquivo para contagem
         String text = "";
+        int counterBreak = 0;
 
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 
         while (br.ready()) {
             text = text.concat(br.readLine());
+            counterBreak++;
         }
-        //Termina aqui
 
         char[] strArray = text.toCharArray();
 
@@ -30,14 +29,12 @@ public class Huffman {
             }
         }
 
+        map.put((char)10,counterBreak);
+
         // Mostra na tela
         for (Map.Entry entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
-
         return map;
-
     }
-
-
 }
