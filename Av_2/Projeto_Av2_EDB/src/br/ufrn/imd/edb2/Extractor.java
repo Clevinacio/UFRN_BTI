f6d43cf6eaf0540dd8e2bc3ff0adc5f6a7cd73eb
jordelucas <jordevalucas@hotmail.com>
2019-10-14 14:56:22 -0300

    cria BitSet a partir do texto codificado

--- Av_2/Projeto_Av2_EDB/src/br/ufrn/imd/edb2/Extractor.java
+++ Av_2/Projeto_Av2_EDB/src/br/ufrn/imd/edb2/Extractor.java
@@ -1,17 +1,22 @@
 package br.ufrn.imd.edb2;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Extractor {
 
     private HashMap<String, Character> mapCodeTable;
+    private BitSet codedText;
     private String fileName;
 
     public Extractor(String fileName){
         this.mapCodeTable = new HashMap<String, Character>();
         this.fileName = fileName;
+        codedText = new BitSet();
     }
 
     /*
@@ -45,7 +50,35 @@ public class Extractor {
     /*
         Cria BitSet a partir do arquivo codificado
      */
-    public void makeBitSet() throws FileNotFoundException {
-        FileInputStream fis = new FileInputStream("convert.edz");
+    public void makeBitSet() throws IOException {
+        byte[] array = Files.readAllBytes(Paths.get("convert.edz"));                //guarda todos os byste do arquivo em um array
+
+//        System.out.println("Imprime o array de bytes");
+//        for (byte b : array) {
+//            System.out.print(Integer.toBinaryString(b & 255 | 256).substring(1));
+//        }
+//        System.out.println("");
+
+        BitSet temp = new BitSet();                                                  //converte os bytes para BitSet temporario
+        for (int i = 0; i < array.length * 8; i++) {
+            if ((array[array.length - i / 8 - 1] & (1 << (i % 8))) > 0) {
+                temp.set(i);
+            }
+        }
+
+        for(int i = 0; i < temp.length(); i++){                                      //inverte a ordem do BitSet temporario e armazena no BitSet da classe
+            this.codedText.set(i, temp.get(temp.length() - i - 1));
+        }
+
+//        System.out.println("Imprime o BitSet");
+//        String value = "";
+//        for (int i = 0; i < codedText.length(); i++) {
+//            if(codedText.get(i) == false) {
+//                value += "0";
+//            }else{
+//                value += "1";
+//            }
+//        }
+//        System.out.println(value);
     }
 }
