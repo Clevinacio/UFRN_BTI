a342670461137b4b4555c3840088620bbf9448de
jordelucas <jordevalucas@hotmail.com>
2019-10-19 11:26:51 -0300

    add dicionario

---
+++ Av_2/Projeto_Av2_EDB/src/br/ufrn/imd/dicionario/Compressor.java
@@ -0,0 +1,110 @@
+package br.ufrn.imd.dicionario;
+
+import java.io.*;
+import java.util.*;
+
+public class Compressor {
+
+    private HashMap<Character, String> mapCodeTable;
+    private String fileName;
+
+    public Compressor(String fileName) throws FileNotFoundException{
+        mapCodeTable = new HashMap<Character, String>();
+        this.fileName = fileName;
+    }
+
+    public Compressor() {
+        mapCodeTable = new HashMap<Character, String>();
+    }
+
+    public HashMap<Character, String> getMapCodeTable() {
+        return mapCodeTable;
+    }
+
+    public void makeCodificationMap(String symbolTable) throws IOException {
+        char[] charArray;                   //array para guardar cada linha do arquivo
+        char letter = '0';                  //guarda caracter ao qual o codigo se refere
+        String code = "";                   //guarda o codigo para o caracter
+        int position = 0;                   //utilizado para verificacao da primeira posicao do array
+        BufferedReader br = new BufferedReader(new FileReader(symbolTable));
+
+        while (br.ready()) {
+            charArray = br.readLine().toCharArray();
+            position = 0;
+            code = "";
+            for (char caracter : charArray) {           //percorrer os caracteres de cada linha
+                if (position == 0) {                    //o primeiro caracter refere-se a letra
+                    letter = caracter;
+                    position++;
+                }else{                                  //demais posicoes referem-se ao codigo
+                    code = code + caracter;
+                }
+            }
+
+            mapCodeTable.put(letter, code);             //o codigo e o caracter sao adicionados ao dicionario
+        }
+    }
+
+    public void CompressTextToFile(String outPutFile) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(this.fileName));
+        BitSet txtCode = new BitSet();
+        String code;
+        int charCurrent = 0;
+        int position = 0;
+
+        while ((charCurrent = br.read()) != -1) {                    //leitura char - char no arquivo
+            if (charCurrent == (char)10) {                           //Se for uma quebra de linha, busca pelo codigo predeterminado
+                code = mapCodeTable.get((char) 280);
+            } else {
+                code = mapCodeTable.get((char) charCurrent);         //senao, busca pelo codigo do caracter corrente
+            }
+
+          /*Se náo encontrar o código no map, o retorno será null. Isso ocorre quando
+            o SO reconhece o /r/n. Só está sendo adicionado o codigo do /n.*/
+            if (code == null) {
+                continue;
+            }
+
+            for (int i = 0; i < code.length(); i++) {       //percorre caracteres do codigo
+                if (code.charAt(i) == '1') {
+                    txtCode.set(position, true);            //atribui valor 1 no bitset
+                }
+                position++;
+            }
+        }
+
+        code = mapCodeTable.get((char) 300);            //Busca EOF
+        for (int i = 0; i < code.length(); i++) {       //Adiciona ao binario
+            if (code.charAt(i) == '1') {
+                txtCode.set(position, true);
+            }
+            position++;
+        }
+
+        while (position % 8 != 0) {                    //para que seja multiplo de 8, as posicioes que sobrarem depois do EOF sao preenchidas com 1
+            txtCode.set(position, true);
+            position++;
+        }
+
+        OutputStream os = new FileOutputStream(new File(outPutFile));
+        os.write(convertBitSetToArray(txtCode));
+        os.close();
+    }
+
+    private byte[] convertBitSetToArray(BitSet txtCode) {
+        byte[] bytes = new byte[(txtCode.length() + 7) / 8];
+        for (int i = 0; i < txtCode.length(); i++) {
+            if (txtCode.get(i)) {
+                bytes[i / 8] |= 1 << (7 - i % 8);
+            }
+        }
+        return bytes;
+    }
+
+    public void porcent(String fileConvert) {
+        File fileBinary = new File(fileConvert);
+        File fileTxt = new File(this.fileName);
+        float porcent = (fileBinary.length() * 100)/fileTxt.length();
+        System.out.printf("arquivo foi possui %.1f%% do seu tamanho original\n", porcent);
+    }
+}
---
+++ Av_2/Projeto_Av2_EDB/src/br/ufrn/imd/dicionario/Extractor.java
@@ -0,0 +1,114 @@
+package br.ufrn.imd.dicionario;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.BitSet;
+import java.util.HashMap;
+
+public class Extractor {
+
+    private HashMap<String, Character> mapCodeTable;
+    private BitSet codedText;
+    private String fileName;
+    private char EOF;
+    private int breakLine;
+
+    public Extractor(String fileName){
+        this.mapCodeTable = new HashMap<String, Character>();
+        this.fileName = fileName;
+        this.codedText = new BitSet();
+        this.EOF = (char) 300;
+        this.breakLine = 280;
+    }
+
+    /*
+        Cria tabela de codificacao a partir do arquivo informado
+     */
+    public void makeCodificationTable () throws IOException {
+        char[] charArray;                   //array para guardar cada linha do arquivo
+        char letter = '0';                  //guarda caracter ao qual o codigo se refere
+        String code = "";                   //guarda o codigo para o caracter
+        int position = 0;                   //utilizado para verificacao da primeira posicao do array
+
+        BufferedReader br = new BufferedReader(new FileReader(this.fileName));
+
+        while (br.ready()) {
+            charArray = br.readLine().toCharArray();
+            position = 0;
+            code = "";
+            for (char caracter : charArray) {           //percorrer os caracteres de cada linha
+                if (position == 0) {                    //o primeiro caracter refere-se a letra
+                    letter = caracter;
+                    position++;
+                }else{                                  //demais posicoes referem-se ao codigo
+                    code = code + caracter;
+                }
+            }
+
+            mapCodeTable.put(code, letter);             //o codigo e o caracter sao adicionados ao dicionario
+        }
+    }
+
+    /*
+        Cria BitSet a partir do arquivo codificado
+     */
+    public void makeBitSet(String compressedFile) throws IOException {
+        byte[] array = Files.readAllBytes(Paths.get(compressedFile));                //guarda todos os byste do arquivo em um array
+
+        BitSet temp = new BitSet();                                                  //converte os bytes para BitSet temporario
+        for (int i = 0; i < array.length * 8; i++) {
+            if ((array[array.length - i / 8 - 1] & (1 << (i % 8))) > 0) {
+                temp.set(i);
+            }
+        }
+
+//        verifica se todos os bits foram passados
+        int resto = temp.length() % 8;
+        int valor;
+        if (resto == 0) {
+            valor = 0;
+        }else{
+            valor = 8 - resto;
+        }
+
+        for(int i = 0; i < temp.length(); i++){                                      //inverte a ordem do BitSet temporario e armazena no BitSet da classe
+            this.codedText.set(i+valor, temp.get(temp.length() - i - 1));
+        }
+    }
+
+    /*
+        Converte BitSet em String - (decodificacao)
+     */
+    public void convertBitSetToString (String outPutFile) throws FileNotFoundException {
+        String buffer = "";                                         //Guarda texto decodificado
+        String code = "";                                           //Guarda codigo para busca no map
+        char letter;                                                //Guarda resultado do map
+
+        for (int i = 0; i < this.codedText.length(); i++) {         //Percorre BitSet
+            if(codedText.get(i) == false) {
+                code += "0";
+            }else{
+                code += "1";
+            }
+
+            if (mapCodeTable.get(code) != null) {                   //Se nao encontrar referencia no map (null) devecontinuar adicionando bits em code
+                letter = mapCodeTable.get(code);                    //Caso encontre, o caracter é guardado
+                if (letter == this.EOF) {                                //É feira a verificacao se o caracter é o EOF
+                    break;
+                }
+
+                if (letter == (char)breakLine) {                         //Se não for o EOF, verifica quebra de linhas
+                    buffer = buffer + "\n";
+                } else {
+                    buffer = buffer + letter;
+                }
+                code = "";
+            }
+        }
+
+        PrintWriter writer = new PrintWriter(outPutFile);
+        writer.print(buffer);
+        writer.close();
+    }
+}
