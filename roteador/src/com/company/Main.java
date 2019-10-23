69643eba22a0abf00f2fd72df0d8eaffb692a3d2
Raul Santos <raul.soutos96@gmail.com>
2019-10-23 17:29:12 -0300

    introducao a atividade - tarefas

--- roteador/src/com/company/Main.java
+++ roteador/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /*12 – Deverão ser criados 9 roteadores. Os roteadores deverão está ligados através de uma matriz 3x3.
+
+          13 – Os pacotes devem vir provenientes de um arquivo onde estarão os roteadores fontes, roteadores destino
+          a quantidade de pacotes a serem enviados nessa comunicação e o conteúdo de cada pacote.
+        */
+
+
     }
 }
--- roteador/src/com/company/Roteador.java
+++ roteador/src/com/company/Roteador.java
@@ -1,9 +1,19 @@
 package com.company;
 
 public class Roteador extends DispositivoDeRede implements Roteamento{
-    //Portas (deve conter um buffer/fila que guarde tipo Pacotes enquanto o arquivo não sai do roteador)
-    //Buffers
+    //Portas - Atributo
+    /*(deve conter um buffer/fila que guarde tipo Pacotes enquanto o arquivo não sai do roteador)
+      Haverão 4 portas para comunicação entre eles mesmos + 1 porta rede (quando chegar no roteador destino
+      é por essa porta que esses pacotes deverão passar)*/
 
-    public Pacote roteamento(Pacote Pacote){}
+    //Buffers - Atributo
 
+    public Pacote roteamento(Pacote Pacote){
+        return Pacote;
+    }
+
+    /* QUANDO O PACOTE CHEGA NO ROTEADOR DESTINO1:
+     Quando os pacotes chegarem no seu respectivo destino, devem sair pela porta correspondente e ter o conteúdo
+     de cada pacote que chega gravado em um arquivo com o nome ip.txt, onde ip é o número IP do roteador corresponde.
+     */
 }
