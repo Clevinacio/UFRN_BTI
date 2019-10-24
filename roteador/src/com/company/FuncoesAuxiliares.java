948a8dd586d64dd9bbe0ffd3e87d6ba258e2384d
Cleverton <clev.inacio@gmail.com>
2019-10-23 23:42:06 -0300

    Cria atribuirCoordenadas na classe FuncoesAuxiliares

--- roteador/src/com/company/FuncoesAuxiliares.java
+++ roteador/src/com/company/FuncoesAuxiliares.java
@@ -24,4 +24,15 @@ public class FuncoesAuxiliares {
             }
         }
     }
+
+
+    public void atribuirCoordenadas(Roteador[][] roteadores) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                roteadores[i][j].setColX(i);
+                roteadores[i][j].setColY(j);
+                System.out.println(roteadores[i][j].getColX()+","+roteadores[i][j].getColY());
+            }
+        }
+    }
 }
