a8132bf62011e633c196df4d78a868b1ea9c0dc5
Cleverton <clev.inacio@gmail.com>
2019-11-02 12:14:31 -0300

    Ajustes arquivos roteador

--- roteador/src/com/company/Main.java
+++ roteador/src/com/company/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Roteador roteadores[][] = new Roteador[3][3];
         FuncoesAuxiliares f1 = new FuncoesAuxiliares();
-
         f1.inicializar(roteadores);
         f1.atribuirIps(roteadores);
         f1.atribuirCoordenadas(roteadores);
