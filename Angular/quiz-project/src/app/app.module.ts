adff709b04ef2361264e881105552a5b1b7ee6fb
Cleverton <cleverton.bezerra@imd.ufrn.br>
2021-06-28 12:58:25 -0300

    Cria quiz em angular

--- Angular/quiz-project/src/app/app.module.ts
+++ Angular/quiz-project/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    QuestionComponent,
+    QuizComponent,
+    ResultsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
